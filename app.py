cbdab110adfc91c92ea3584e335ef0834d7b6b07
machine <53405109+machine1010@users.noreply.github.com>
2025-10-10 01:51:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import time
 import tempfile
 from pathlib import Path
 import streamlit as st
-from dummy_processor import run_pipeline
+from dummy_processor import runpipeline
 
 # Set page config with wide layout
 st.set_page_config(
